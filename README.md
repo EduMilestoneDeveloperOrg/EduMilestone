d97c5ab3991c2d91ddd64154de4550d7e0ad0611
Education Milestone Developer <EduMilestone.developer.Primary@hotmail.com>
2025-03-02 12:06:46 +0530

    First Commit
    First Commit from Owner

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # EduMilestone
+EduMilestone is an educational app.
+The first Independent Heavy Milestone Modules Services, contain major services the Tools Section Product, includes features (Independent micro services) like OCR, PDF Viewer, Word Processing, 
+PDF Editing, PPT Tools, and many more.
+We’re developing OCR (Version 01 Release) first, followed by more features.
+Milestone Modules 01 → Tools Section Product (Contains multiple independent microservices like OCR, PDF Viewer, Word Processing, etc).
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EduMilestone"
+include(":app")
+include(":milestone")
+include(":milestone:milestone_modules_01")
